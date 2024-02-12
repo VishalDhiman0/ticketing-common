a0311d026ece3e7a4e6e9cccbd232725ab4ee9e8
vishaldhiman <vishaldhiman01999@gmail.com>
2024-02-12 21:33:14 +0530

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 interface Color {
     red: number;
     blue: number;
+    green: number;
 }
 
 const color: Color = {
     red: 23,
-    blue: 23
+    blue: 23,
+    green: 32
 }
 
 console.log(color);
